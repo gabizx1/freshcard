135304c734971ebfcf27b581af6c96787de95bea
Gabizinha123 <137187002+gabizx1@users.noreply.github.com>
2023-07-04 11:13:25 -0300

    Add files via upload

--- services/database.py
+++ services/database.py
@@ -1,1 +1,11 @@
+import psycopg2
 
+#Fazendo a conexão com o banco de dados
+con = psycopg2.connect(
+    host='localhost',
+    database='frechcard',
+    user='postgres',
+    password='pabd')
+
+#Curso da conexão
+cur = con.cursor()
