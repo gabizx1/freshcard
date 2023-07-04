4482a126ca3e1fa736768fa6371529efc58c5a6b
Gabizinha123 <137187002+gabizx1@users.noreply.github.com>
2023-07-04 11:13:08 -0300

    Add files via upload

--- page/insert.py
+++ page/insert.py
@@ -2,19 +2,22 @@ import streamlit as st
 
 import controller.cliente as cliente
 
-def editar():
-    st.title('EDITAR FLASHCARD')
+def inserir():
+    st.title('CRIAR FLASHCARD')
     
     with st.form(key='insert'):
-        input_id = st.text_input(label='Insira o ID:',)
+        input_name = st.text_input(label='Insira o nome:')
+        input_age = st.number_input(label='Insira a matrícula:', format='%d', step=1)
         input_per = st.text_input(label='Insira a pergunta:',)
         input_res = st.text_input(label='Insira a resposta:',)
         input_ass = st.text_input(label='Insira o assunto:',)
+        input_serie = st.text_input(label='Insira a serie:',)
         input_materia = st.text_input(label='Insira a matéria:',)
 
         
         buttom_submit = st.form_submit_button('Enviar')
         
         if buttom_submit:
-            cliente.editar(input_id, input_per,input_res,input_ass,input_materia)
-            st.success('Cliente alterado com sucesso')
+            cliente.incluir(input_name, input_age, input_per,input_res,input_ass,input_serie,input_materia)
+            st.success('Cliente incluido com sucesso')
+            
